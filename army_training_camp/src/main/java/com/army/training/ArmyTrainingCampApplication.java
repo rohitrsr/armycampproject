84b57f15627be07c2ae25e735ac4540ad44adcff
ROHIT <ROHIT@DESKTOP-HJJQUBJ>
2020-08-25 11:21:33 +0530

    FirstCommit(25-Aug-20)

---
+++ army_training_camp/src/main/java/com/army/training/ArmyTrainingCampApplication.java
@@ -0,0 +1,13 @@
+package com.army.training;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArmyTrainingCampApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArmyTrainingCampApplication.class, args);
+	}
+
+}
