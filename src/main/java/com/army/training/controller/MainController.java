aa0c224ccc83b002f42b442b637479c7789dd5da
unknown <rrathour.rohit23@gmail.com>
2020-08-25 20:42:51 +0530

    third commit 25-aug-20

---
+++ src/main/java/com/army/training/controller/MainController.java
@@ -0,0 +1,29 @@
+package com.army.training.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+	@GetMapping("/index")
+	public String index() {
+		return "index";
+	}
+
+	@GetMapping("gallery")
+	public String gallery() {
+		return "gallery";
+	}
+	
+	@GetMapping("armyEligibility")
+	public String armyEligibilityPage() {
+		return "armyEligibility";
+	}
+	
+	@GetMapping("policeEligibility")
+	public String policeEligibilityPage() {
+		return "policeEligibility";
+	}
+
+}
---
+++ src/test/java/com/army/training/ArmyTrainingCampApplicationTests.java
@@ -0,0 +1,13 @@
+package com.army.training;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArmyTrainingCampApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
